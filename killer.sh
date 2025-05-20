bf1b661138adfc8503de65c94a95965a6e363fcb
qunzhong <1155210998@link.cuhk.edu.hk>
2025-05-20 11:31:30 +0800

    .

--- killer.sh
+++ killer.sh
@@ -3,7 +3,7 @@
 # 输入参数
 pid=$1  # 已知任务的 PID，从命令行传入
 cd /m2v_intern/wangqunzhong/research/ddpo-pytorch
-next_task="source rl_auxscripts/rl_sh/"$2"gpu_rl.sh --entropy_loss=True --reward_cot=False"  # 下一个任务
+next_task="rl_auxscripts/rl_sh/"$2"gpu_rl.sh"  # 下一个任务
 
 # 检查 PID 是否存在
 if ! ps -p $pid > /dev/null; then
@@ -15,7 +15,7 @@ echo "Managing task with PID: $pid"
 
 # 等待 3 小时
 echo "Waiting for 3 hours before stopping the task... and start $next_task"
-sleep 3s
+sleep 3h
 
 # 杀掉指定的任务
 echo "Stopping task with PID: $pid"
@@ -31,4 +31,4 @@ fi
 
 # 启动下一个任务
 echo "Starting next task: $next_task"
-$next_task
+source $next_task  --entropy_loss=True --reward_cot=False
